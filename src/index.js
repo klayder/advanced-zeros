467f33edd5c87e554ffe61d422580a6f807fbab6
Klayder <anderysorokin@gmail.com>
2018-02-20 19:32:13 +0300

    almost done 87/100
    need fix choice of multiplier

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  function getSimpleMultipliers(num){
+	let tempArr=[],tempObj={};
+	
+	for(let i =2; i<=num;i++){
+		if(num%i === 0){
+			tempObj[i]=1;
+			num=num/i;
+		}
+
+		while(num%i ===0){
+			num=num/i;
+			tempObj[i]++;
+		}
+		
+	}
+	return tempObj;
 }
+ var multipliers=getSimpleMultipliers(base);
+for(let key in multipliers){
+	var multiplier=Number(key);
+
+}
+
+var conter=0;
+k=multiplier;
+  	while(k<number || k==number){
+   		var tempK=k;
+    
+      	while(tempK%multiplier === 0){
+        		conter++;
+      			tempK/=multiplier;
+        }
+        
+				
+      k+=multiplier;
+    }
+return Math.round(conter/multipliers[multiplier]);
+}
+
+
+
+
